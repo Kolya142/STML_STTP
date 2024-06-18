e46ce10b64cea9f9113a1929eb4bb428d2dfcbb5
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-06-18 19:47:09 +0300

    Summary

---
+++ STTP/target/debug/STTPs.d
@@ -0,0 +1,1 @@
+C:\Users\sony\Documents\STML_STTP\STTP\target\debug\STTPs.exe: C:\Users\sony\Documents\STML_STTP\STTP\src\main.rs
---
+++ STTP/target/debug/deps/STTPs.d
@@ -0,0 +1,5 @@
+C:\Users\sony\Documents\STML_STTP\STTP\target\debug\deps\STTPs.exe: src\main.rs
+
+C:\Users\sony\Documents\STML_STTP\STTP\target\debug\deps\STTPs.d: src\main.rs
+
+src\main.rs:
