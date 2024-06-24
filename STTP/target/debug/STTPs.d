4a828f0099fd34141c9fecea9124d7993a424d9f
Kolya <87275865+Kolya142@users.noreply.github.com>
2024-06-24 14:27:00 +0300

    Delete STTP/target directory

--- STTP/target/debug/STTPs.d
+++
@@ -1,1 +0,0 @@
-C:\Users\sony\Documents\STML_STTP\STTP\target\debug\STTPs.exe: C:\Users\sony\Documents\STML_STTP\STTP\src\main.rs
--- STTP/target/debug/deps/STTPs-b989406a84993a7e.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sony\Documents\STML_STTP\STTP\target\debug\deps\STTPs-b989406a84993a7e.rmeta: src\main.rs
-
-C:\Users\sony\Documents\STML_STTP\STTP\target\debug\deps\STTPs-b989406a84993a7e.d: src\main.rs
-
-src\main.rs:
